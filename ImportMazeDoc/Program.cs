ff6b40950e7c642454b8ac45d4aa28edb65ab938
Frey Clante <fclante@gmail.com>
2017-03-10 21:05:32 +0100

    Initial commit, made 2D array

--- ImportMazeDoc/Program.cs
+++ ImportMazeDoc/Program.cs
@@ -15,55 +15,43 @@ namespace ImportMazeDoc
             string path = Directory.GetCurrentDirectory();// Efter at man har lavet sin .txt Skal man placere den i current working directory.
             Console.WriteLine("The current directory is {0}", path);// Det viser metodekladet, det gemmes og skrives ud.
 
-            string Content = File.ReadAllText("MazeTest1.txt");
-            string resourcetext1 = Resource1.MazeTest1;
-            Console.Out.NewLine = "\r\n\r\n";
-            Console.WriteLine("This is the text file read in and The string printed \n{0}", Content);
-            Console.WriteLine("same file loaded using resources \n\r{0}", resourcetext1);*/
-
-            string[] Lines = File.ReadAllLines("MazeTest1.txt");
-            char[] Lines2 = 
-            Console.Out.NewLine = "\r\n\r\n";
-
-            foreach (string line in Lines)
-            {
-                Console.WriteLine(line); 
-            }
-            
-            Console.WriteLine("This is the resource char array");
-            foreach (char line in Lines2)
-            {
-                Console.WriteLine(line);
-            }
-
-            int arraySize = Lines.Length;
-            string array2Size = arraySize.ToString();
-
-            Console.WriteLine("Array Size: {0}", array2Size);
-            Console.WriteLine(Lines[0]);
-
-            Console.Out.NewLine = "\r\n\r\n";
-
-
+            string Content = File.ReadAllText("maze.txt");
+          //  Console.Out.NewLine = "\r\n\r\n";
             Console.WriteLine("This is the text file read in and The string printed \n{0}", Content);
 
+            Console.WriteLine();
+            Console.WriteLine("Output of new 2D array Maze!");
+            Console.WriteLine();
 
-            string[,] lines2D = new string[21,21];
+            string[] Lines = File.ReadAllLines("maze.txt");
 
+            int width = Lines[1].Length;
+            int height = Lines.Length - 1;
 
-            for (int i = 0; i < lines2D.Length; i++)
+            string[,] mazeArr = new string[height, width];
+
+            for (int i = 1; i < Lines.Length; i++)
             {
-
-
-
-                for (int j = 0; j < length; j++)
+                for (int j = 0; j < Lines[i].Length; j++)
                 {
+                    mazeArr[i - 1, j] = Lines[i][j].ToString(); // Uses Indexer get() method.
+                }
+            }
 
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width ; j++)
+                {
+                    Console.Write(mazeArr[i, j]);
                 }
 
+                Console.WriteLine();
             }
 
             
+
+
         }
     }
+
 }
